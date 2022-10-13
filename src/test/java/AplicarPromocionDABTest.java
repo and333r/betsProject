069754204201fb7a06454d462949139785babd63
Mikel Lonbide <100244220+Mikeloon@users.noreply.github.com>
2022-10-13 17:57:46 +0200

    Update AplicarPromocionDABTest.java

--- src/test/java/AplicarPromocionDABTest.java
+++ src/test/java/AplicarPromocionDABTest.java
@@ -1,12 +1,15 @@
 
-
 import static org.junit.Assert.assertEquals;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.TypedQuery;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import dataAccess.DataAccess;
@@ -17,17 +20,16 @@ import test.dataAccess.TestDataAccess;
 public class AplicarPromocionDABTest {
 	
 	//sut:system under test
-	 static DataAccess sut=new DataAccess();
+	 static DataAccess sut = new DataAccess();
 	 
 	 //additional operations needed to execute the test 
-	 static TestDataAccess testDA=new TestDataAccess();
+	 static TestDataAccess testDA = new TestDataAccess();
 	 
 	 @Test	 //Caso donde nos salta la excepcion cuando nos llega un parametro nulo.
 	 public void test1() {
-			
-		 try {
+		 Usuario u1 = new Usuario("Lonbas","12351789N", new ArrayList<Promocion>());
+		 try {  
 				
-				Usuario u1 = new Usuario("Mikel", new ArrayList<Promocion>());
 				String text = null;
 				testDA.open();
 				testDA.anadirUsuario(u1);
@@ -41,6 +43,9 @@ public class AplicarPromocionDABTest {
 				testDA.close();
 				
 			}catch(NullPointerException e ) {
+				testDA.open();
+				testDA.borrarUsuario(u1);
+				testDA.close();
 				e.printStackTrace();
 				fail();
 			} 
@@ -53,16 +58,11 @@ public class AplicarPromocionDABTest {
 				
 				Usuario u1 = null;
 				String text = "a";
-				testDA.open();
-				testDA.anadirUsuario(u1);
-				testDA.close();
+				
 				
 				int resul = sut.aplicarPromocion(text, u1);
 				assertEquals(3,resul);
 				
-				testDA.open();
-				testDA.borrarUsuario(u1);
-				testDA.close();
 				
 			}catch(NullPointerException e ) {
 				e.printStackTrace();
@@ -72,13 +72,15 @@ public class AplicarPromocionDABTest {
 	 
 	 @Test //Caso donde ya se han utilizado toda la cantidad de veces la promocion, devuelve 2
 	 public void test3() {
+		 	
+		 	Promocion p = new Promocion("LaLiga ofertas",new ArrayList<Usuario>(),0);
+			Usuario u1 = new Usuario("Ander", "12356789N", new ArrayList<Promocion>());
+			Usuario u2 = new Usuario("ale","12356789a", new ArrayList<Promocion>());
+			p.anadirUsuario(u2);
 		 
-		 try {
-		String text = "a";
-		Promocion p = new Promocion("a",1);
-		Usuario u1 = new Usuario("Mikel", new ArrayList<Promocion>());
-		Usuario u2 = new Usuario("Lon", new ArrayList<Promocion>());
+		try {
 		 
+		 String text = "LaLiga ofertas";
 		 testDA.open();
 		 int comp = testDA.anadirPromocion(p);
 		 System.out.println(comp);
@@ -87,9 +89,9 @@ public class AplicarPromocionDABTest {
 		 testDA.close();
 		 
 		 int resul = sut.aplicarPromocion(text, u1);
-		 int resul2 = sut.aplicarPromocion(text, u2);
+		 System.out.println(resul);
 		 
-		 assertEquals(2,resul2);
+		 assertEquals(2,resul);
 			
 			
 			testDA.open();
@@ -99,6 +101,11 @@ public class AplicarPromocionDABTest {
 			testDA.close();
 			
 		 } catch(Exception e) {
+			 	testDA.open();
+				testDA.borrarUsuario(u1);
+				testDA.borrarUsuario(u2);
+				testDA.borrarPromocion(p);
+				testDA.close();
 			 e.printStackTrace();
 			 fail();
 		 } 
@@ -107,77 +114,78 @@ public class AplicarPromocionDABTest {
 	 
 	 @Test //Caso donde el usuario ya ha utilizado el codigo, devuelve 1
 	 public void test4() {
+		 Promocion p = new Promocion("LaLiga offer",new ArrayList<Usuario>(),0);
+			Usuario u1 = new Usuario("Ander", "12356789h", new ArrayList<Promocion>());
+			p.anadirUsuario(u1);
 		 
-		 try {
-		 Promocion p = new Promocion("0");
-		 ArrayList<Promocion> promociones = new ArrayList <Promocion>();
-		 promociones.add(p);
-		 String text = "a";
-		 ArrayList<Usuario> users = new ArrayList <Usuario>();
-		 Usuario u = new Usuario("0");
-		 users.add(u);
+		try {
 		 
-		 Usuario u1 = new Usuario("A", promociones);
-		 users.add(u1);
-		 Promocion p2 = new Promocion(text, users);
-		 
-		 
-		 
+		 String text = "LaLiga offer";
 		 testDA.open();
-		 testDA.anadirPromocion(p2);
+		 int comp = testDA.anadirPromocion(p);
+		 System.out.println(comp);
 		 testDA.anadirUsuario(u1);
 		 testDA.close();
 		 
 		 int resul = sut.aplicarPromocion(text, u1);
+		 System.out.println(resul);
 		 
 		 assertEquals(1,resul);
-			assertTrue(true);
+			
 			
 			testDA.open();
-			testDA.borrarPromocion(p2);
 			testDA.borrarUsuario(u1);
+			testDA.borrarPromocion(p);
 			testDA.close();
 			
 		 } catch(Exception e) {
-			 System.out.println(e.getMessage());
+			 	testDA.open();
+				testDA.borrarUsuario(u1);
+				testDA.borrarPromocion(p);
+				testDA.close();
+			 e.printStackTrace();
+			 fail();
 		 } 
-		 	 
+		 	
 	 }
 	 
 	 @Test //Caso donde todo va bien, devuelve 0
 	 public void test5() {
+		 Promocion p = new Promocion("LaLiga ofer",new ArrayList<Usuario>(),2);
+			Usuario u1 = new Usuario("pablo", "12356789l", new ArrayList<Promocion>());
+			Usuario u2 = new Usuario("alejandra","12356789a", new ArrayList<Promocion>());
+			p.anadirUsuario(u2);
 		 
-		 try {
-		 Promocion p = new Promocion("0");
-		 ArrayList<Promocion> promociones = new ArrayList <Promocion>();
-		 promociones.add(p);
-		 String text = "a";
-		 ArrayList<Usuario> users = new ArrayList <Usuario>();
-		 Usuario u = new Usuario("0");
-		 users.add(u);
+		try {
 		 
-		 Usuario u1 = new Usuario("A", promociones);
-		 Promocion p2 = new Promocion(text, users);
-		 
-		 
-		 
+		 String text = "LaLiga ofer";
 		 testDA.open();
-		 testDA.anadirPromocion(p2);
+		 int comp = testDA.anadirPromocion(p);
+		 System.out.println(comp);
 		 testDA.anadirUsuario(u1);
+		 testDA.anadirUsuario(u2);
 		 testDA.close();
 		 
 		 int resul = sut.aplicarPromocion(text, u1);
+		 System.out.println(resul);
 		 
 		 assertEquals(0,resul);
-			assertTrue(true);
+			
 			
 			testDA.open();
-			testDA.borrarPromocion(p2);
 			testDA.borrarUsuario(u1);
+			testDA.borrarUsuario(u2);
+			testDA.borrarPromocion(p);
 			testDA.close();
 			
 		 } catch(Exception e) {
-			 System.out.println(e.getMessage());
+			 	testDA.open();
+				testDA.borrarUsuario(u1);
+				testDA.borrarUsuario(u2);
+				testDA.borrarPromocion(p);
+				testDA.close();
+			 e.printStackTrace();
+			 fail();
 		 } 
 		 	 
 	 }
