48172ea2b4d8d92f6d9006979ae0a69db5f7624c
Ander <andercaroescan9@gmail.com>
2022-11-13 17:24:56 +0100

    Patrón 1 acabado

---
+++ src/main/java/factory/BLFactory.java
@@ -0,0 +1,105 @@
+package factory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.UIManager;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import businessLogic.BLFacade;
+import businessLogic.BLFacadeImplementation;
+import configuration.ConfigXML;
+import configuration.UtilDate;
+import dataAccess.DataAccess;
+import domain.Actor;
+import domain.Usuario;
+
+public class BLFactory {
+	
+	ConfigXML c = ConfigXML.getInstance();
+
+	
+	public void BLFactory(){}
+	
+	
+	public BLFacade getBusinessLogicFactory() throws MalformedURLException {
+		BLFacade appFacadeInterface ;
+		
+		if(c.isBusinessLogicLocal()) {
+			appFacadeInterface= getLocalBusinessLogicFactory();
+		}
+		else {
+			appFacadeInterface= getWebBusinessLogicFactory();
+		}
+		return appFacadeInterface;
+	}
+
+
+	private BLFacade getWebBusinessLogicFactory() throws MalformedURLException {
+		BLFacade appFacadeInterface ;
+		String serviceName= "http://"+c.getBusinessLogicNode() +":"+ c.getBusinessLogicPort()+"/ws/"+c.getBusinessLogicName()+"?wsdl";
+		 
+		//URL url = new URL("http://localhost:9999/ws/ruralHouses?wsdl");
+		URL url = new URL(serviceName);
+		
+        //1st argument refers to wsdl document above
+		//2nd argument is service name, refer to wsdl document above
+//        QName qname = new QName("http://businessLogic/", "FacadeImplementationWSService");
+        QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
+        Service service = Service.create(url, qname);
+
+        appFacadeInterface = service.getPort(BLFacade.class);
+        
+        return appFacadeInterface;
+        
+		
+	}
+
+
+	private BLFacade getLocalBusinessLogicFactory() {
+		BLFacade appFacadeInterface ;
+		
+		DataAccess da= new DataAccess(c.getDataBaseOpenMode().equals("initialize"));
+		
+		Usuario usuario1 = new Usuario("Usuario1", "11111111F", "usuario", "primero", "primero", UtilDate.nuevaFecha(2001,10,25), "usuario", 'M', "usuario@gmail.com", "675253642");
+		System.out.println("hola");
+		System.out.println(da.obtenerUsuarioDeseado(usuario1));
+		appFacadeInterface =new BLFacadeImplementation(da);
+		
+		return appFacadeInterface;
+		
+	}
+	
+	
+}
+
+
+/*
+ * 
+		try {
+			
+			BLFacade appFacadeInterface;
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+			
+			appFacadeInterface= getBLFactory()
+			
+			
+			else { //If remote
+				
+				 String serviceName= "http://"+c.getBusinessLogicNode() +":"+ c.getBusinessLogicPort()+"/ws/"+c.getBusinessLogicName()+"?wsdl";
+				 
+				//URL url = new URL("http://localhost:9999/ws/ruralHouses?wsdl");
+				URL url = new URL(serviceName);
+
+		 
+		        //1st argument refers to wsdl document above
+				//2nd argument is service name, refer to wsdl document above
+//		        QName qname = new QName("http://businessLogic/", "FacadeImplementationWSService");
+		        QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
+		 
+		        Service service = Service.create(url, qname);
+
+		        appFacadeInterface = service.getPort(BLFacade.class);
+			} 
+ */
--- src/main/java/gui/ApplicationLauncher.java
+++ src/main/java/gui/ApplicationLauncher.java
@@ -10,6 +10,7 @@ import javax.xml.ws.Service;
 
 import configuration.ConfigXML;
 import dataAccess.DataAccess;
+import factory.BLFactory;
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
 
@@ -38,39 +39,12 @@ public class ApplicationLauncher {
 		
 		try {
 			
-			BLFacade appFacadeInterface;
+			BLFacade appFacadeInterface= new BLFactory().getBusinessLogicFactory();
 //			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
 //			UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 			
-			if (c.isBusinessLogicLocal()) {
-				
-				//In this option the DataAccess is created by FacadeImplementationWS
-				//appFacadeInterface=new BLFacadeImplementation();
-
-				//In this option, you can parameterize the DataAccess (e.g. a Mock DataAccess object)
-
-				DataAccess da= new DataAccess(c.getDataBaseOpenMode().equals("initialize"));
-				appFacadeInterface =new BLFacadeImplementation(da);
-			}
 			
-			else { //If remote
-				
-				 String serviceName= "http://"+c.getBusinessLogicNode() +":"+ c.getBusinessLogicPort()+"/ws/"+c.getBusinessLogicName()+"?wsdl";
-				 
-				//URL url = new URL("http://localhost:9999/ws/ruralHouses?wsdl");
-				URL url = new URL(serviceName);
-
-		 
-		        //1st argument refers to wsdl document above
-				//2nd argument is service name, refer to wsdl document above
-//		        QName qname = new QName("http://businessLogic/", "FacadeImplementationWSService");
-		        QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
-		 
-		        Service service = Service.create(url, qname);
-
-		        appFacadeInterface = service.getPort(BLFacade.class);
-			} 
 			/*if (c.getDataBaseOpenMode().equals("initialize")) 
 				appFacadeInterface.initializeBD();
 				*/
